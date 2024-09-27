f4935685d47e6502eb68491ca0610ce28b9fbb62
Binamra Shrestha <binamra08@gmail.com>
2024-09-27 10:47:53 +0100

    test push

---
+++ src/Card.tsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img alt="poopoo" src="../public/vite.svg                   "/>
+            <h2>Patients</h2>
+            <p>stopo</p>
+        </div>
+    );
+}
+
+export default Card
